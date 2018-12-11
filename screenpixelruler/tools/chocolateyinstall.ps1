621e8f10b7e92813a4766f4c87d21f6fb35731af
Stewart Cossey <stewart.cossey@gmail.com>
2018-12-11 21:03:29 +1300

    Add ScreenPixelRuler.

---
+++ screenpixelruler/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$target = Join-Path $toolsDir "ScreenPixelRuler.exe"
+Install-ChocolateyShortcut -shortcutFilePath "$([Environment]::GetFolderPath('CommonStartMenu'))\Programs\Screen Pixel Ruler.lnk" -targetPath "$target" -description "A pixel perfect on screen ruler."
+
+
