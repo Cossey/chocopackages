db299ce2be8571fd4a8e7cd7cf14d5570474a2b8
Stewart Cossey <stewart.cossey@gmail.com>
2019-01-07 02:35:28 +1300

    Corrective action to remove comments from files and also remove 64 bit url. Update Burnaware Free to 11.9.

--- burnawarefree/tools/chocolateyinstall.ps1
+++ burnawarefree/tools/chocolateyinstall.ps1
@@ -1,20 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.burnaware.com/downloads/burnaware_cfree_11.8.exe' # download url, HTTPS preferred
+$url        = 'https://www.burnaware.com/downloads/burnaware_cfree_11.9.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE' 
   url           = $url
-  url64bit      = $url
 
-  softwareName  = 'BurnAware Free*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'BurnAware Free*'
 
-  checksum      = '3C9303E92B50722813CD87D46D55C8A3E171DBE0B5B96CF640D76DBBD83BD848'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = '3C9303E92B50722813CD87D46D55C8A3E171DBE0B5B96CF640D76DBBD83BD848'
-  checksumType64= 'sha256' #default is checksumType
+  checksum      = '4DB16B8FD6A1AB9736EFBB02A07EAF4C03A7669D6526359C1D55F44C3134D6BC'
+  checksumType  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
--- burnawarepro/tools/chocolateyinstall.ps1
+++ burnawarepro/tools/chocolateyinstall.ps1
@@ -1,20 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.burnaware.com/downloads/burnaware_pro_11.8.exe' # download url, HTTPS preferred
+$url        = 'https://www.burnaware.com/downloads/burnaware_pro_11.8.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE' 
   url           = $url
-  url64bit      = $url
 
-  softwareName  = 'BurnAware Free*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'BurnAware Professional*'
 
   checksum      = 'EE8F3A8E7C28959D5B73FAC2D50418DC41AD0466EE285603835C9372D8C0AA0E'
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = 'EE8F3A8E7C28959D5B73FAC2D50418DC41AD0466EE285603835C9372D8C0AA0E'
-  checksumType64= 'sha256' #default is checksumType
+  checksumType  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
