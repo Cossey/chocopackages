d4d51f2ada3c80a0e485509103759b8625f70ffd
Stewart Cossey <stewart.cossey@gmail.com>
2019-02-22 14:43:40 +1300

    Update git ignore files Stop tracking burnaware free files Add script for automatically building Burnaware Free package from website.

---
+++ build-burnawarefree.ps1
@@ -0,0 +1,67 @@
+Write-Host "Building Chocolately Package for Burnaware Free..."
+
+Write-Host "Getting current version..."
+$currentversionurl = "www.burnaware.com/download.html"
+$currentversion = Invoke-WebRequest -Uri $currentversionurl 
+$version = [regex]::match($currentversion.Content, "BurnAware Free.*Version (.*?)<br />", [Text.RegularExpressions.RegexOptions]::Singleline).Groups[1].Value
+Write-Host "Version: $version"
+$url = [regex]::match($currentversion.Content, "blockquote class=`"well`".*?href=`"(.*?)`"", [Text.RegularExpressions.RegexOptions]::Singleline).Groups[1].Value
+if ($url.StartsWith("/")) { $url = "https://www.burnaware.com" + $url }
+Write-Host "Download Link: $url"
+
+Write-Host "Downloading file to get the filehash..."
+$whatsnewurl = "www.burnaware.com/whats-new.html"
+$outfile = ".\temp\free_$version"
+Invoke-WebRequest -Uri $url -outfile $outfile
+$filehash = (Get-FileHash $outfile).Hash
+Write-Host "File hash: $filehash"
+Remove-Item –path $outfile
+
+Write-Host "Getting changelog info..."
+$whatsnew = Invoke-WebRequest -Uri $whatsnewurl 
+
+$releasedate = [regex]::match($whatsnew.Content, "Released (.*)<br />").Groups[1].Value
+
+$newfeatures = [regex]::match($whatsnew.Content, "New Features </span>(.*?)</p>", [Text.RegularExpressions.RegexOptions]::Singleline).Groups[1].Value
+$newfeatures = $newfeatures -replace " &#8226;", "*"
+$newfeatures = $newfeatures -replace "<br />", ""
+$newfeatures = $newfeatures -replace "<br/>", ""
+
+$enhancements = [regex]::match($whatsnew.Content, "Enhancements </span>(.*?)</p>", [Text.RegularExpressions.RegexOptions]::Singleline).Groups[1].Value
+$enhancements = $enhancements -replace " &#8226;", "*"
+$enhancements = $enhancements -replace "<br />", ""
+$enhancements = $enhancements -replace "<br/>", ""
+
+$bugfixes = [regex]::match($whatsnew.Content, "Bug Fixes </span>(.*?)</p>", [Text.RegularExpressions.RegexOptions]::Singleline).Groups[1].Value
+$bugfixes = $bugfixes -replace " &#8226;", "*"
+$bugfixes = $bugfixes -replace "<br />", ""
+$bugfixes = $bugfixes -replace "<br/>", ""
+Write-Host "Release date $releasedate"
+
+
+Write-Host "Processing install file..."
+$citemplate = Get-Content ".\templates\burnawarefree\chocolateyInstall.ps1.template" -raw
+
+$citemplate = $citemplate -replace "%hash%", "$filehash"
+$citemplate = $citemplate -replace "%downloadurl%", "$url"
+
+$citemplate | Out-File ".\burnawarefree\tools\chocolateyInstall.ps1"
+
+Write-Host "Processing nuspec file..."
+$nstemplate = Get-Content ".\templates\burnawarefree\burnawarefree.nuspec.template" -raw
+$nstemplate = $nstemplate -replace "%fileversion%", "$version"
+$nstemplate = $nstemplate -replace "%releasedate%", "$releasedate"
+$nstemplate = $nstemplate -replace "%newfeatures%", "$newfeatures"
+$nstemplate = $nstemplate -replace "%enhancements%", "$enhancements"
+$nstemplate = $nstemplate -replace "%bugfixes%", "$bugfixes"
+$nstemplate | Out-File ".\burnawarefree\burnawarefree.nuspec"
+
+Write-Host "Creating choco package..."
+cd .\burnawarefree
+cpack
+Write-Host "Uploading choco package..."
+cpush
+Write-Host "Delete choco package..."
+del *.$version.nupkg
+cd ..
+Write-Host "Script completed"
